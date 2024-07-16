1a880583751d262ccf3f8afe953c80c14dbab520
Jan Nogue <jan.nogue@qilimanjaro.tech>
2024-07-16 10:14:50 +0200

    solved warnings in rl_agent

--- rl-zx/rl_agent.py
+++ rl-zx/rl_agent.py
@@ -6,6 +6,7 @@ import torch_geometric.nn as geom_nn
 
 from torch.distributions.categorical import Categorical
 from torch_geometric.nn import Sequential as geo_Sequential
+from torch_geometric.nn.aggr import AttentionalAggregation
 
 class CategoricalMasked(Categorical):
     def __init__(self, probs=None, logits=None, validate_args=None, masks=None, device="cpu"):
@@ -47,7 +48,7 @@ class AgentGNN(nn.Module):
         c_in_v = 12 #dimension value obs
         edge_dim = 7
         edge_dim_v = 2
-        self.global_attention_critic = geom_nn.GlobalAttention(
+        self.global_attention_critic = AttentionalAggregation(
             gate_nn=nn.Sequential(
                 nn.Linear(c_hidden, c_hidden),
                 nn.LeakyReLU(),
@@ -204,11 +205,12 @@ class AgentGNN(nn.Module):
             action_id = torch.tensor([0]).to(device)
             
         if testing:
-            return action.T, action_id.T
+            return action.permute(*torch.arange(action.ndim - 1, -1, -1)), action_id.permute(*torch.arange(action_id.ndim - 1, -1, -1))
         
         logprob = categoricals.log_prob(action)
         entropy = categoricals.entropy(device)
-        return action.T, logprob, entropy, values, torch.tensor(action_logits).to(device).reshape(-1, 1), action_id.T
+        return action.permute(*torch.arange(action.ndim - 1, -1, -1)), logprob, entropy, values, action_logits.clone().detach().to(device).reshape(-1, 1), action_id.permute(*torch.arange(action_id.ndim - 1, -1, -1))
+        
 
     def get_value(self, x):
         values = self.critic(x)
